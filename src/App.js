c00a6747841bbca4b35c842ab76ebf4d5aadf3d3
KarlStevenRequinto <lerdrobott@gmail.com>
2024-03-08 03:56:20 +0800

    initialize to git

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 import React from "react";
 
-function App() {
-    return <h1 className="underline text-3xl">APP</h1>;
-}
+const App = () => {
+    return <h1 className="underline text-3xl">App</h1>;
+};
 
 export default App;
