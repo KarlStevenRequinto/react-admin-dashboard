5f20f4afb467c2e402a52d763a21935bc9fad04c
KarlStevenRequinto <lerdrobott@gmail.com>
2024-03-09 02:22:46 +0800

    fixing icon position

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,9 +13,8 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => {
     return (
         <TooltipComponent content={title} position="BottomCenter">
             <button type="button" onClick={customFunc} style={{ color }} className="relative text-xl rounded-full p-3 hover:bg-light-gray">
-                <span style={{ background: dotColor }} className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2">
-                    {icon}
-                </span>
+                <span style={{ background: dotColor }} className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2" />
+                {icon}
             </button>
         </TooltipComponent>
     );
@@ -39,7 +38,7 @@ const Navbar = () => {
             setActiveMenu(true);
         }
     }, [screenSize]);
-    
+
     return (
         <div className="flex justify-between p-2 md:mx-6 relative">
             <NavButton title="Menu" customFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)} color="blue" icon={<AiOutlineMenu />} />
