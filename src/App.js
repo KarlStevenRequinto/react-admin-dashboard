d4c2509ed7040ed2c8d3270abd72d0865339e8a9
KarlStevenRequinto <lerdrobott@gmail.com>
2024-03-11 15:54:57 +0800

    fix dark and light mode

--- src/App.js
+++ src/App.js
@@ -25,10 +25,10 @@ import {
 
 import { useStateContext } from "./contexts/ContextProvider";
 const App = () => {
-    const { activeMenu, themeSettings, setThemeSettings } = useStateContext();
+    const { activeMenu, themeSettings, setThemeSettings,currentColor,currentMode } = useStateContext();
 
     return (
-        <div>
+        <div className={currentMode === "Dark" ? "dark" : ""}>
             <BrowserRouter>
                 <div className="flex relative dark:bg-main-dark-bg">
                     <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
@@ -36,7 +36,7 @@ const App = () => {
                             <button
                                 type="button"
                                 className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
-                                style={{ background: "blue", borderRadius: "50%" }}
+                                style={{ background:currentColor, borderRadius: "50%" }}
                                 onClick={() => setThemeSettings(true)}
                             >
                                 <FiSettings />
@@ -54,7 +54,7 @@ const App = () => {
                         </div>
                     )}
 
-                    <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? "md:ml-72" : "flex-2"}`}>
+                    <div className={`dark:bg-main-dark-bg bg-main-bg min-h-screen w-full ${activeMenu ? "md:ml-72" : "flex-2"}`}>
                         <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
                             <Navbar />
                         </div>
